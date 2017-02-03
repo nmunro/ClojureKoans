d91bb6a3970de6e44a11b145b2942dff01806fb8
Neil Munro <neilmunro@gmail.com>
2017-02-03 11:25:49 +0000

    Added the implementation of 027.

--- zero-two-seven/src/zero_two_seven/core.clj
+++ zero-two-seven/src/zero_two_seven/core.clj
@@ -2,7 +2,13 @@
   (:gen-class))
 
 (defn pal [ls]
-  false)
+  ; Need to use cons
+  (loop [old-list ls new-list []]
+    (if (empty? old-list)
+      (if (= new-list ls)
+        true
+        false)
+      (recur (rest old-list) (cons (first old-list) new-list)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
